53a107a61d32820cc146b8275c548d06f58190e6
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-06-10 12:56:31 +0000

    fix: test

--- src/App.test.ts
+++ src/App.test.ts
@@ -3,6 +3,6 @@ import { App } from './App';
 
 describe('App', () => {
   it('renders hello world', () => {
-    expect(App()).toContain('Start typing...');
+    expect(App()).toContain('<canvas id="triangleCanvas"></canvas>');
   });
 });
